3e6c8557173c33cc57393ed4eb19af297b8f72ce
emberhawk <70652115+emberhawk@users.noreply.github.com>
2021-02-24 14:38:42 +0000

    Updated Movement
    Updated and customized player movement with keyboard

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     [Header("Player Movement")]
-    [SerializeField] [Range(0,1f)] float xValue = 0.01f;
     [SerializeField] [Range(0,1f)] float yValue = 0f;
-    [SerializeField] [Range(0,1f)] float zValue = 0f;
-
+    [SerializeField] [Range(1f, 20f)] float moveSpeed = 5f;
 
     void Start()
     {
@@ -19,6 +17,11 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
+      var delta = Time.deltaTime;
+
+      float xValue = Input.GetAxis("Horizontal") * delta * moveSpeed;
+      float zValue = Input.GetAxis("Vertical") * delta * moveSpeed;
+
       transform.Translate(xValue, yValue, zValue);
 
 
